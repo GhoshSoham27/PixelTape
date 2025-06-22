fd1a277a657b15545a3fc454af27f5162a49fe74
Soham <gsoham101gmail.com>
2025-06-22 16:58:37 +0530

    started w auth

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,12 +1,19 @@
 import Header from '@/components/Header';
+import VideoCard from '@/components/VideoCard';
+import { dummyCards } from '@/constants';
 import React from 'react'
 
 const page = async ({params}: ParamsWithSearch) => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const {id} = await params;
   return (
     <div className='wrapper page'>
         <Header subHeader="gsoham101gmail.com" title="Soham | Chillboy Services" userImg="/assets/images/dummy.jpg"/>
-        <h1 className='text-2xl font-karla'>USER ID: {id}</h1>
+        <section className='video-grid'>
+          {dummyCards.map((card) => (
+            <VideoCard key={card.id} {...card} />
+          ))}
+        </section>
     </div>
   )
 }
