06389e2cfde3fa765fd9e7dca70222cb310f7561
Soham <gsoham101@gmail.com>
2025-06-22 17:05:20 +0530

    fixed commit

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -2,4 +2,4 @@ import { getXataClient } from "@/xata";
 import { drizzle } from "drizzle-orm/xata-http";
 
 const xata = getXataClient();
-export const db = drizzle(xata);
+export const db = drizzle(xata); 
